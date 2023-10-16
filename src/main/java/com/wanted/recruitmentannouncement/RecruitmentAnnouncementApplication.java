f4db16bf0d2416d00a328e5f2d30e399ff936412
parkjungmin <aossuper7@naver.com>
2023-10-16 10:05:43 +0900

    initial commit

---
+++ src/main/java/com/wanted/recruitmentannouncement/RecruitmentAnnouncementApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.recruitmentannouncement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAnnouncementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAnnouncementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/recruitmentannouncement/RecruitmentAnnouncementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.recruitmentannouncement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAnnouncementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
