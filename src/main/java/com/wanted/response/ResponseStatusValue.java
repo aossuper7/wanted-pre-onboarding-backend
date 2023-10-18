f4b1526af541921efd0cc6097634b890a0a3812e
parkjungmin <aossuper7@naver.com>
2023-10-18 22:00:45 +0900

    refactor: 에러코드 수정

--- src/main/java/com/wanted/response/ResponseStatusValue.java
+++ src/main/java/com/wanted/response/ResponseStatusValue.java
@@ -11,9 +11,9 @@ public enum ResponseStatusValue {
     /**
      * 400 : Request, Response 오류
      */
-    NOT_FOUND_RECRUITMENT(false, "404", "채용 공고를 찾을 수 없습니다."),
-    NOT_FOUND_MEMBER(false, "404", "유저를 찾을 수 없습니다."),
-    DUPLICATE_APPLICATION(false, "409", "사용자당 한번만 지원 할 수 있습니다.");
+    NOT_FOUND_RECRUITMENT(false, "REC400", "채용 공고를 찾을 수 없습니다."),
+    NOT_FOUND_MEMBER(false, "MBR400", "유저를 찾을 수 없습니다."),
+    DUPLICATE_APPLICATION(false, "APP400", "사용자당 한번만 지원 할 수 있습니다.");
 
     private final boolean isSuccess;
     private final String code;
